ba3b7ce6be6cf7355d9fb163352cfe74d0fe601e
hualeel <hualeel@sina.com>
2021-01-02 02:47:50 +0800

    init

--- main.py
+++ main.py
@@ -18,10 +18,9 @@ app = Flask(__name__, template_folder="templates")
 @app.route('/list-pods', methods=["GET"])
 @cross_origin()
 def get_pod_ip():
-    k8s_api_server = str(os.getenv("K8S_API_SERVER"))
-    # k8s_api_auth = str(os.getenv("K8S_API_AUTH"))
-    k8s_api_auth = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImpMNFdRVzhCOWRuN3BlWjZkbkFRYVUtZDZjN1dhWWd6cHhibTFQMnNuLVUifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJrdWJlLXN5c3RlbSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJhZG1pbi10b2tlbi1qOW5xYyIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50Lm5hbWUiOiJhZG1pbiIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6ImZhZmZlMzEzLWY3YzQtNDhmMC05YTZlLWI1OGUzYjEyNGIyZCIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDprdWJlLXN5c3RlbTphZG1pbiJ9.c_j1Xhl5QORXya-DnHHZSLzMxyg48B0PIuyFIRDU4L-YsN-Etzey55s6m6DVw9YZKvRlY9NtD0On2pS4uYwFabuPjzydJ0U08MixaVMuchb2e29FhsD0buW3tQp8V3OSL4KVmXAO3yMcJr1utd3LQa2aM4YclmO3Os_DRNyKGTm3D5LjUJeXY4CdaikG8-AhC7JPd-idx_akIjeVDUn7ShhAuTOlJEs6vFiYMdkcW_O3yYNuon1ACPQ01zm70eEWsjNXiJZrotCJ2Us2ItqmV93hcVMjPKGROJwjzul7oFIZIWwaZu3xy83oLwOCjzSYypDJOPGHAyovw_clpbaWiA"
-    k8s_namespace = str(os.getenv("K8S_NAMESPACE"))
+    k8s_api_server = os.getenv("K8S_API_SERVER")
+    k8s_api_auth = os.getenv("K8S_API_AUTH")
+    k8s_namespace = os.getenv("K8S_NAMESPACE")
 
     # url = "https://" + k8s_api_server + ":6443/api/v1/namespaces/" + k8s_namespace + "/endpoints/" + svc_name
     url = "https://" + k8s_api_server + ":6443/apis/apps/v1/namespaces/" + k8s_namespace + "/deployments"
