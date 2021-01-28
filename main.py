ca71379d9fa6557d8e458e4ba348dd967035206b
hualeel <hualeel@sina.com>
2021-01-28 20:44:10 +0800

    init

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__, template_folder="templates")
 
 
 # 获取pod ip list
-@app.route('/list-pods', methods=["GET"])
+@app.route('/', methods=["GET"])
 @cross_origin()
 def get_pod_ip():
     k8s_api_server = os.getenv("K8S_API_SERVER")
